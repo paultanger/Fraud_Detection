59b4b74d532e46046086caa77bf918972ca61264
rickyk9487 <rickyk9487@gmail.com>
2015-02-27 12:35:07 -0800

    new version

--- register.py
+++ register.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask,request
 import flask_restful as restful
 from apscheduler.schedulers.blocking import BlockingScheduler
 import os
@@ -10,87 +10,100 @@ from threading import Thread
 from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
+import logging
 import pandas as pd
-import sys
+
+logging.basicConfig()
+
+apps = {}
+
+
+urls = []
+
+
+
+def start_server():
+    if 'apps' in apps:
+        return
+    try:
+        apps['apps'] = Flask(__name__)
+        api = restful.Api(apps['apps'])
+        api.add_resource(Register, '/register')
+        http_server = HTTPServer(WSGIContainer(apps['apps']))
+        http_server.listen(5000,'0.0.0.0')
+    except:
+        pass
+
+def get_object():
+    random_json = data.loc[[random.randint(0,num_data_points)]].to_json()
+    as_dict =  simplejson.loads(random_json)
+    ret = {}
+    first_key = None
+    for key in as_dict.keys():
+        if(first_key is None):
+            first_key = as_dict.itervalues().next().keys()[0]
+        ret[key] = as_dict[key][first_key]
+    return ret
+
+
+def ping():
+    if 'apps' not in apps:
+        start_server()
+        thread = Thread(target = IOLoop.instance().start)
+        thread.start()
+
+    for url in urls:
+        print 'url',url
+
+        random_json = get_object()
+        headers = {'Content-Type': 'application/json'}
+        try:
+            post(url + '/score',data=simplejson.dumps(random_json),headers=headers)
+            print "sent to %s" % url
+        except ConnectionError:
+            print "%s not listening" % url
+
 
 
 class Register(restful.Resource):
+
     def post(self):
         ip = request.form['ip']
         port = request.form['port']
-        url = 'http://%s:%s' % (ip, port)
-        Server.urls.add(url)
-        return {'Message': 'Added url %s' % url}
+        url = 'http://' + ip + ':' + port
+        if url not in urls:
+            urls.append(url)
+        else:
+            print 'Url ' + url + ' is already registered'
+        return {'Message': 'Added url ' + url}
 
     def put(self):
-        print request.json
+
+        print  request.json
         return simplejson.dumps(request.json)
 
 
-class Server(object):
-    urls = set()
 
-    def __init__(self):
-        self.scheduler = None
-        self.data = None
-        self.app = None
 
-    def load_data(self, filename, label='acct_type'):
-        data = pd.read_json(filename)
-        data.drop(label, axis=1, inplace=True)
-        return data
+with open('data/test_new.json') as f:
+    contents = f.read()
+    data = pd.read_json('data/test_new.json')
+    for column in data.columns:
+        if column == 'acct_type':
+              data.drop(column, axis=1, inplace=True)
+    num_data_points = data.shape[0]
+    print get_object()
 
-    def start_server(self):
-        try:
-            app = Flask(__name__)
-            api = restful.Api(app)
-            api.add_resource(Register, '/register')
-            http_server = HTTPServer(WSGIContainer(app))
-            http_server.listen(5000, '0.0.0.0')
-            return app
-        except Exception as e:
-            print e.message
-
-    def get_random_datapoint(self):
-        index = random.randint(0, len(self.data))
-        datapoint = self.data.loc[[index]].to_dict()
-        for key, value in datapoint.iteritems():
-            datapoint[key] = value.values()[0]
-        return index, datapoint
-
-    def ping(self):
-        if not self.app:
-            self.app = self.start_server()
-            thread = Thread(target=IOLoop.instance().start)
-            thread.start()
-            print "Server started. Ready to receive posts."
-        index, datapoint = self.get_random_datapoint()
-        print "sending datapoint %d" % index
-        for url in self.urls:
-            headers = {'Content-Type': 'application/json'}
-            try:
-                post(url + '/score',
-                     data=simplejson.dumps(datapoint),
-                     headers=headers)
-                print "sent to %s" % url
-            except ConnectionError as e:
-                print "%s not listening." % url
-
-    def run(self, filename='test.json'):
-        self.data = self.load_data(filename)
-        self.scheduler = BlockingScheduler(timezone=pytz.utc)
-        self.scheduler.add_job(self.ping, trigger='interval', seconds=5)
-        print 'Press Ctrl+C to exit'
-        try:
-            self.scheduler.start()
-        except:
-            print "\nGoodbye."
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print "Usage: python register.py filename.json"
-        exit()
-    filename = sys.argv[1]
-    server = Server()
-    server.run(filename)
+    scheduler = BlockingScheduler(timezone=pytz.utc)
+
+    #10 seconds from now
+    scheduler.add_job(ping, 'interval', seconds=30, max_instances =100)
+    print('Press Ctrl+{0} to exit'.format('Break' if os.name == 'nt' else 'C'))
+
+    try:
+        scheduler.start()
+    except (KeyboardInterrupt, SystemExit):
+        pass
