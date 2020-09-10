1e67a4e6caec56b2a35cce4f6159099bb659b82b
Gina Santana <endernova@Ginas-MacBook-Pro.local>
2020-09-10 14:19:39 -0600

    Updated model_draft file to include train, test, split

--- src/model_draft.py
+++ src/model_draft.py
@@ -4,25 +4,40 @@ import matplotlib.pyplot as pyplot
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.metrics import confusion_matrix
 
-def Random_forest_model(X, y):
-    rf_model = RandomForestClassifier().fit(X,y)
-    return rf_model.score(X,y)
+def Random_forest_model(X, y, num_trees, num_features):
+    X_train, X_test, y_train, y_test = train_test_split(X, y, straify=y) 
+    rf_model = RandomForestClassifier().fit(X_train, y_train, n_estimators=num_trees, max_features=num_features)
+    y_predict = rf_model.predict(X_test)
+    confusion_matrix = confusion_matrix(y_test, y_pred)
+    return rf_model.score(X_test,y_test), confusion_matrix
 
 
 def Logistic_reg_model(X,y):
-    LR = LogisticRegression().fit(X,y)
-    return LR.score(X,y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
+    LR = LogisticRegression().fit(X_train, y_train)
+    y_predict = LR.predict(X_test)
+    confusion_matrix = confusion_matrix(y_test, y_pred)
+    return LR.score(X_test, y_test), confusion_matrix
 
 def KN_model(X,y, neigh):
-    N_model = KNeighborsClassifier(n_neighbors=neigh).fit(X,y)
-    return N_model.score(X,y)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, stratify=y)
+    N_model = KNeighborsClassifier(n_neighbors=neigh).fit(X_train,y_train)
+    y_predict = N_model.predict(X_test)
+    confusion_matrix = confusion_matrix(y_test, y_pred)
+    return N_model.score(X_test,y_test), confusion_matrix
+
+def ROC_curve():
+    pass
 
 if __name__=='__main__':
     X = #placeholder
     y = #placeholder
 
-    Random_forest_model(X,y)
+    Random_forest_model(X,y,#placeholder, #placehodler)
     Logistic_reg_model(X,y)
     KN_model(X,y,#placeholder)
      
