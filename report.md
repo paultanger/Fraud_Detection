70b1a4166c525e9eae160b2099bbbb97e9c41ae5
Paul Tanger <detroitstylz@gmail.com>
2020-09-11 08:00:28 -0600

    add db and api updates

--- report.md
+++ report.md
@@ -5,6 +5,15 @@ we ran all numerical columns with along the an expaned version of the "ticket ty
 ## preprocessing
 > We took all columns with numerical values and filled nans with the mean of the column. we expanded the "ticket type" column into rows of tickets sold, ticket quantity , average ticket sale price, and ratio of sold to tickets available.  
 
+# comparing fraud and not fraud examples
+We found that many of the numerical variable means were different between the two groups:
+
+| Fraud   |   approx_payout_date |   body_length |   channels |   delivery_method |   event_created |   event_end |   event_published |   event_start |   fb_published |     gts |   has_analytics |   has_header |   has_logo |   name_length |   num_order |   num_payouts |   object_id |   org_facebook |   org_twitter |   sale_duration |   sale_duration2 |   show_map |   user_age |   user_created |   user_type |   venue_latitude |   venue_longitude |
+|:--------|---------------------:|--------------:|-----------:|------------------:|----------------:|------------:|------------------:|--------------:|---------------:|--------:|----------------:|-------------:|-----------:|--------------:|------------:|--------------:|------------:|---------------:|--------------:|----------------:|-----------------:|-----------:|-----------:|---------------:|------------:|-----------------:|------------------:|
+| False   |          1.35125e+09 |       3886.99 |       6.38 |              0.48 |     1.34587e+09 | 1.35082e+09 |       1.34291e+09 |   1.35054e+09 |           0.14 | 2481.68 |            0.08 |         0.21 |       0.86 |         42.78 |       30.34 |         37.04 | 4.51462e+06 |           8.63 |          4.69 |           49.83 |            53.95 |       0.85 |     402.68 |    1.31108e+09 |        2.82 |            35.17 |            -60.69 |
+| True    |          1.34769e+09 |       1508.89 |       4.15 |              0.07 |     1.34533e+09 | 1.34726e+09 |       1.31854e+09 |   1.34678e+09 |           0.02 | 1911.21 |            0    |         0.07 |       0.65 |         29.98 |        4.53 |          0.58 | 4.70004e+06 |           1.04 |          0.29 |           14.13 |            16.54 |       0.76 |      87.15 |    1.3378e+09  |        1.62 |            35.97 |            -43.41 |
+
+
 
 ## accuracy metrics selected
 f1 score: harmonic mean of recal and percision
--- src/api_client.py
+++ src/api_client.py
@@ -2,6 +2,7 @@
 import time
 import requests
 import pymongo
+import pandas as pd
 
 
 class EventAPIClient:
@@ -18,10 +19,57 @@ class EventAPIClient:
         self.api_key = api_key
         self.db = db
         self.interval = 30
+        # paul added
+        self.cols = ['body_length',
+                'channels',
+                'country',
+                'currency',
+                'delivery_method',
+                'description',
+                'email_domain',
+                'event_created',
+                'event_end',
+                'event_published',
+                'event_start',
+                'fb_published',
+                'has_analytics',
+                'has_header',
+                'has_logo',
+                'listed',
+                'name',
+                'name_length',
+                'object_id',
+                'org_desc',
+                'org_facebook',
+                'org_name',
+                'org_twitter',
+                'payee_name',
+                'payout_type',
+                'previous_payouts',
+                'sale_duration',
+                'show_map',
+                'ticket_types',
+                'user_age',
+                'user_created',
+                'user_type',
+                'venue_address',
+                'venue_country',
+                'venue_latitude',
+                'venue_longitude',
+                'venue_name',
+                'venue_state',
+                'sequence_number']
+        self.df = pd.DataFrame(columns = self.cols)
 
     def save_to_database(self, row):
         """Save a data row to the database."""
-        print("Received data:\n" + repr(row) + "\n")  # replace this with your code
+        #print("Received data:\n" + repr(row) + "\n")  # replace this with your code
+        # print(pd.json_normalize(row))
+        self.df = self.df.append(pd.json_normalize(row))
+        self.df.to_csv('../data/api_data.csv', index=False)
+    
+    def get_df(self):
+        return self.df
 
     def get_data(self):
         """Fetch data from the API."""
@@ -54,4 +102,39 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # main()
+    # do it manually to access df
+    client = EventAPIClient()
+    client.collect()
+    df = client.get_df()
+    print(df)
+    # or just get most recent 10
+    # client = EventAPIClient()
+    # recent10 = client.get_data()
+    # this is a list of json records?
+    # print(type(recent10))
+    # print(len(recent10))
+    #print(recent10)
+    # we can convert to df:
+    # record = pd.json_normalize(recent10) 
+    # print(record)
+    # collect will keep writing them to db once we implement that
+
+    # then we need to call our prediction function on the data in the db
+    # and update the fraud col?
+    # or maybe we need two tables.. one for API data and one to store updated
+    # in the notes below, it says it will come as a string, but seems like
+    # it comes as a dict?
+
+    # should we modify class to include connection to our db?
+
+    ### IMPORTANT !!!! (after verifying that the data hasn't been seen before).
+    '''
+    The individual example will no longer be coming from a local file, 
+    but instead you will get it by making a request to a server that will give 
+    you a data point as a string, which you can parse into JSON. You can use 
+    json.loads() to parse a string to json, which is the reverse process of json.dumps(). 
+    You'll still need to vectorize it, predict, and store the example and prediction 
+    in the database.
+    '''
+
