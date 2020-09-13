d1d9815b4d08a9595c06a1a88a68cb6b6fd877e6
Paul Tanger <detroitstylz@gmail.com>
2020-09-13 11:44:31 -0600

    update app with new pages

--- web_app/app.py
+++ web_app/app.py
@@ -2,15 +2,39 @@ from flask import Flask, request, render_template
 import pickle
 import numpy as np
 import pandas as pd
+from sqlalchemy import create_engine
 from getpass import getpass
+
+# from flask_sqlalchemy import SQLAlchemy
+# db = SQLAlchemy(app)
+
 # from src.api_client import EventAPIClient
 # #---     use pickle to load in the pre-trained model; loaded at the top of the app, loaded into memory ONCE on the server rather than loaded every time
 # with open(f'','rb') as f:
 #     model = pickle.load(f)
 
-app = Flask(__name__, root_path='./') # template_folder = 'templates/')
+def setup_db(db_details):
+    return create_engine(db_details)
+
+def run_api(db_details):
+    client = EventAPIClient(db=db_details)
+    client.collect()
+
+# setup db for queries
+# db_details = f'postgresql://postgres:{getpass()}@52.15.236.214:5432/fraud_data'
+db_details = f'postgresql://postgres:galvanize@52.15.236.214:5432/fraud_data'
+engine = setup_db(db_details)
 
 x = pd.read_csv('../data/display.csv')
+
+app = Flask(__name__, root_path='./') # template_folder = 'templates/')
+
+# remove template limit
+# app.jinja_env.cache = {}
+
+# a more complex guide here: https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
+# https://stackabuse.com/using-sqlalchemy-with-flask-and-postgresql/
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -36,10 +60,40 @@ def analysis():
     return render_template("analysis.html", tables=[x.to_html(classes='data')], titles=x.columns.values)
 
 # display most recent 10 from api
-@app.route('/recent10') 
+@app.route('/recent10')
 def recent10():
-    return render_template("recent10.html", tables=[x.to_html(classes='data')], titles=x.columns.values)
+    query = "SELECT * FROM api_data WHERE created_at IS NOT NULL ORDER BY created_at DESC LIMIT 10;"
+    try:
+        rows = pd.read_sql(query, con=engine)
+    except:
+        return f"""something is broken"""
 
+    return render_template('recent10.html', tables=[rows.to_html(classes='data')], titles=rows.columns.values)
+
+# let user choose some parameters on what to query
+
+@app.route('/query_form')
+def query_form():
+    # form action is what to do when submitted
+    return ''' enter the number of recent records from the API to view <form action="/query_results" method="POST">
+                <input type="text" name="n_records" />
+                <input type="submit" />
+               </form>
+             '''
+
+@app.route('/query_results', methods=['POST'])
+def query_results():
+    try:
+        n_records = int(request.form['n_records'])
+        query = f"SELECT body_length, channels, country, currency, delivery_method, description, email_domain, \
+                fb_published, has_analytics, name, org_name, \
+                sale_duration, user_age, venue_country, venue_name, created_at \
+                FROM api_data WHERE created_at IS NOT NULL ORDER BY created_at DESC LIMIT {n_records};"
+        rows = pd.read_sql(query, con=engine)
+    except:
+        return f"""something is broken"""
+
+    return render_template('query_results.html', tables=[rows.to_html(classes='data')], titles=rows.columns.values)
 
 # custom 404
 @app.errorhandler(404)
@@ -47,14 +101,11 @@ def page_not_found(e):
     # note that we set the 404 status explicitly
     return render_template('404.html'), 404
 
-def run_api(db_details):
-    client = EventAPIClient(db=db_details)
-    client.collect()
-
 if __name__ == '__main__':
     # setup api save to db
     # db_details = f'postgresql://postgres:{getpass()}@52.15.236.214:5432/fraud_data'
     # run_api(db_details)
+    # run app
     app.run(host='0.0.0.0', port=8080, debug=True)
     # for AWS
     # app.run(host='0.0.0.0', port=33507, debug=False)
