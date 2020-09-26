0c9b66bc478a9244b2874057d67fe5508ffacbaa
Paul Tanger <detroitstylz@gmail.com>
2020-09-26 15:57:12 -0600

    update ip for new aws instance

--- web_app/app.py
+++ web_app/app.py
@@ -5,14 +5,6 @@ import pandas as pd
 from sqlalchemy import create_engine
 from getpass import getpass
 
-# from flask_sqlalchemy import SQLAlchemy
-# db = SQLAlchemy(app)
-
-# from src.api_client import EventAPIClient
-# #---     use pickle to load in the pre-trained model; loaded at the top of the app, loaded into memory ONCE on the server rather than loaded every time
-# with open(f'','rb') as f:
-#     model = pickle.load(f)
-
 def setup_db(db_details):
     return create_engine(db_details)
 
@@ -21,8 +13,7 @@ def run_api(db_details):
     client.collect()
 
 # setup db for queries
-# db_details = f'postgresql://postgres:{getpass()}@52.15.236.214:5432/fraud_data'
-db_details = f'postgresql://postgres:galvanize@52.15.236.214:5432/fraud_data'
+db_details = f'postgresql://postgres:{getpass()}@3.128.75.60:5432/fraud_data'
 engine = setup_db(db_details)
 
 x = pd.read_csv('../data/display.csv')
@@ -31,10 +22,6 @@ app = Flask(__name__, root_path='./') # template_folder = 'templates/')
 
 # remove template limit
 # app.jinja_env.cache = {}
-
-# a more complex guide here: https://realpython.com/flask-by-example-part-2-postgres-sqlalchemy-and-alembic/
-# https://stackabuse.com/using-sqlalchemy-with-flask-and-postgresql/
-
 
 @app.route('/', methods=['GET'])
 def home():
@@ -107,64 +94,10 @@ def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    # setup api save to db
-    # db_details = f'postgresql://postgres:{getpass()}@52.15.236.214:5432/fraud_data'
-    # run_api(db_details)
     # run app
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    if len(sys.argv) > 1:
+        app.run(host='0.0.0.0', port=33507, debug=False)
+    else:
+        app.run(host='0.0.0.0', port=8080, debug=True)
     # for AWS
     # app.run(host='0.0.0.0', port=33507, debug=False)
-
-
-"""
-M notes:
-#----   after cursor add following code to take in new data and put into PD
-#----   take input from SQL and place into Pandas DF
-sql_query = pd.read_sql_query('SELECT * FROM **NAME**, conn)
-
-#----   will need to run sql_query through cleaning function
-from predict import all_together
-
-cleaned_x = all_together(sql_query)
-
-#----   will need to run code above through pickle model
-loaded_model = pickle.load(open('../data/pickle_model.pkl', 'rb'))
-proba = loaded_model.predict_proba(cleaned_x) #array of proba
-proba = list(proba[:,1])
-cleaned_x['prediction'] = proba
-
-#----   get pred.proba and categorize into HIGH, MED, LOW by adding column *WHERE does this info go?
-def flag_label(row):
-    if row['predict'] == 1 and row['predict'] > 0.97:
-        return 'HIGH'
-    elif row['predict'] > 0.95:
-        return 'MEDIUM'
-    elif row['predict'] > 0.91:
-        return 'LOW'
-    else:
-        return 'Not Fraud'
-
-cleaned_x['flag'] = cleaned_x.apply(lambda row: flag_label(row), axis = 1)
-display = cleaned_x.iloc[:,[16, 32]].copy() #DOUBLE CHECK
-
-#----   render above dataframe as html and select 
-#----   show top features and fraud label and specific identification label 
-fraud_html = display.to_html()
-#----   show top features and fraud label and specific identification label 
-fraud_html = display.to_html()
-#----   show top features and fraud label and specific identification label 
-fraud_html = display.to_html()
-#----   show top features and fraud label and specific identification label 
-fraud_html = display.to_html()
-#----   show top features and fraud label and specific identification label 
-fraud_html = display.to_html()
-#----   show top features and fraud label and specific identification label 
-fraud_html = display.to_html()
-#----   show top features and fraud label and specific identification label 
-fraud_html = display.to_html()
-
-#----   write html to flask app
-text_file = open("fraud_check.html", "w")
-text_file.write(html)
-text_file.close()
-"""
